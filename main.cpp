a883ce940e7fe07afb5f7722e1904a73d207b6f2
Serum0017 <joeinfinity@outlook.com>
2025-02-23 21:03:07 -0800

    much rendering progress
    much wow very yes

--- main.cpp
+++ main.cpp
@@ -7,141 +7,95 @@
 #include <functional>
 using namespace std;
 
+#include <sstream>
+#include "render.cpp"
+
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 
-const int canvasW = 1600;
-const int canvasH = 900;
+#include <signal.h>
 
 using namespace emscripten;
 
-int main() {
-    // const auto document = emscripten::val::global("document");
-    // const auto canvas = document.call<emscripten::val, std::string>("querySelector", "canvas");
+std::unordered_map<int, int> instructToMem;
+std::unordered_map<int, int> instructToLength;
 
-    // auto ctx = canvas.call<emscripten::val, std::string>("getContext", "2d");
-  
-    // const auto width = canvas["width"].as<int>();
-    // const auto height = canvas["height"].as<int>();
-  
-    // ctx.call<void>("clearRect", 0, 0, width, height);
-  
-    // // rect
-    // ctx.set("fillStyle", "white");
-    // ctx.call<void>("fillRect", 0, 0, width, height);
-  
-    // // line
-    // ctx.set("strokeStyle", "black");
-    // ctx.call<void>("moveTo", 0, 0);
-    // ctx.call<void>("lineTo", width, height);
-    // ctx.call<void>("stroke");
-  
-    // // text
-    // ctx.set("fillStyle", "black");
-    // ctx.set("font", "bold 48px serif");
-    // ctx.call<void, std::string>("fillText", "Hello World!", width / 2, height / 2);
-  
-    return 0;
-}
+int64_t readDouble(byte*, int64_t);
+void writeDouble(byte*, int64_t, int64_t);
+int64_t opQ(int64_t, int64_t, int64_t);
+bool cmpSuccessful(bool, bool, bool, int);
 
-float rotation = 0;
+byte* program;
+byte memoryData[4096];
+int pc = 0;// program counter
+int64_t registers[15];
+bool zeroFlag = false;
+bool overflowFlag = false;
+bool signedFlag = false;
+int statusCode = 1;
+int stepNum = 0;
 
-void rotateAt(emscripten::val ctx, int x, int y, float angle){
-    ctx.call<void>("translate", x, y);
-    ctx.call<void>("rotate", angle);
-    ctx.call<void>("translate", -x, -y);
-}
-
-const int registerW = 150;
-const int registerH = canvasH / 15;
-const int registerPadding = 5;
-void renderRegister(emscripten::val ctx, int x, int y, long val, string registerName, bool isOdd){
-    rotateAt(ctx, x + registerW/2, y + registerH/2, isOdd ? rotation : -rotation);
-
-    ctx.set("strokeStyle", "blue");
-    ctx.set("lineWidth", 2);
-    ctx.call<void>("strokeRect", x + registerPadding, y + registerPadding, registerW - registerPadding*2, registerH - registerPadding*2);
-
-    // string str = long2str()
-    ctx.set("fillStyle", "black");
-    ctx.set("font", "bold 48px serif");
-    ctx.set("textAlign", "center");
-    ctx.set("textBaseline", "middle");
-    ctx.call<void>("fillText", registerName, x + registerW/2, y + registerH/2);
-    
-    // ctx.call<void>("beginPath");
-    // ctx.set("strokeStyle", "red");
-    // ctx.call<void>("moveTo", x, y);
-    // ctx.call<void>("lineTo", x + registerW, y + registerH);
-    // ctx.call<void>("stroke");
-    // ctx.call<void>("closePath");
-
-    rotateAt(ctx, x + registerW/2, y + registerH/2, isOdd ? -rotation : rotation);
-}
-
-std::string long2str(long w, size_t hex_len = sizeof(long)<<1) {
-    static const char* digits = "0123456789ABCDEF";
-    std::string rc(hex_len,'0');
-    for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
-        rc[i] = digits[(w>>j) & 0x0f];
-    return rc;
-}
-
-void renderRegisters(emscripten::val ctx){
-    const string registerNames[] = {"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r12", "r13"};
-    int x = canvasW/2 - registerW/2;
-    int y = 0;
-    for(int i = 0; i < 15; i++){
-        renderRegister(ctx, x, y, 0/*TODO val*/, registerNames[i], i % 2 == 1);
-        y += registerH;
+std::vector<std::string> split(std::string& s, const std::string& delimiter) {
+    std::vector<std::string> tokens;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        tokens.push_back(token);
+        s.erase(0, pos + delimiter.length());
     }
+    tokens.push_back(s);
+
+    return tokens;
 }
 
-void renderConditionCode(int x, int y, long val, string conditionCodeName){
+int main(){
+    std::ifstream code;
+    code.open("asm.txt");
+    string line;
+    std::getline(code, line);
+
+    std::vector<std::string> hexVals = split(line, " ");
     
-}
-
-void renderPC(int x, int y, long val){
-
-}
-
-void renderStatus(int x, int y, int status){
     
-}
-
-void renderStackTop(int x, int y, long val){
+    int len = hexVals.size();
+    program = new byte[len];
+    for(int i = 0; i < len; i++){
+        unsigned int x;
+        std::stringstream ss;
+        ss << std::hex << hexVals[i];
+        ss >> x;
+        program[i] = (byte) x;
+    }
     
-}
+    instructToLength[0] = 1;
+    instructToLength[1] = 1;
+    instructToLength[2] = 2;
+    instructToLength[3] = 10;
+    instructToLength[4] = 10;
+    instructToLength[5] = 10;
+    instructToLength[6] = 2;
+    instructToLength[7] = 9;
+    instructToLength[8] = 2;
+    instructToLength[9] = 9;
+    instructToLength[10] = 2;
+    instructToLength[11] = 2;
 
-void renderStage(emscripten::val ctx, string stage){
-    // text
-    ctx.set("fillStyle", "black");
-    ctx.set("font", "bold 48px serif");
-    ctx.call<void>("fillText", stage, 20, 20);
-}
+    instructToMem[0] = 0;
+    instructToMem[1] = 0;
+    instructToMem[2] = 2;
+    instructToMem[3] = 2;
+    instructToMem[4] = 2;
+    instructToMem[5] = 0;
+    instructToMem[6] = 1;
+    instructToMem[7] = 0;
+    instructToMem[8] = 1;
+    instructToMem[9] = 0;
+    instructToMem[10] = 0;
+    instructToMem[11] = 0;
 
-void render(float dt) {// deltaTime, use for animations
-    const auto document = emscripten::val::global("document");
-    const auto canvas = document.call<emscripten::val, std::string>("querySelector", "canvas");
-
-    auto ctx = canvas.call<emscripten::val, std::string>("getContext", "2d");
-
-    // render background
-    ctx.set("fillStyle", "white");
-    ctx.call<void>("fillRect", 0, 0, canvasW, canvasH);
-
-    rotation += dt;
-
-    renderRegisters(ctx);
-}
-
-// float lerp(float a, float b, float t) {
-//     return (1 - t) * a + t * b;
-// }
-
-EMSCRIPTEN_BINDINGS(my_module) {
-    // emscripten::function("lerp", &lerp);
-    emscripten::function("render", &render);
+    initRender();
+    return 0;
 }
 
 // int main() {
@@ -169,10 +123,197 @@ EMSCRIPTEN_BINDINGS(my_module) {
 //     return 0;
 // }
 
+std::unordered_map<string, int64_t> fetch(int64_t pc, int* statusCode, byte* program, std::unordered_map<int, int> instructToMem, std::unordered_map<int, int> instructToLength){
+    // FETCH
+    std::unordered_map<string, int64_t> returnDict;
+
+    byte instructionNum = program[pc];
+    int instructionCode = ((int) instructionNum) >> 4;
+    if(instructionCode==0){
+        *statusCode = 2;
+        //raise(SIGSEGV);// CRASH AND BURN
+        return returnDict;
+    }
+    
+    // first and second half of byte[1] of instruction
+    int rA = (int) ((program[pc + 1]) >> 4);
+    int rB = (int) (program[pc + 1] & ((byte) 0xF));
+    int fnCode = (int) ((instructionNum) & ((byte) 0xF));
+
+    int valP = pc+instructToLength[instructionCode];
+    
+    int64_t valC = 0;
+
+    if(instructToMem[instructionCode]!=0){
+        for(int i = 0; i<8; i++){
+            valC += (int64_t) program[(int) pc+instructToMem[instructionCode]+i]; //Hopefully pc is never bigger than 2^32 (PRAY)
+            valC *= 8;
+        }
+    }
+
+    returnDict["rA"] = (int64_t) rA; //Casting to int64_t is OK here, we can just cast back to int later
+    returnDict["rB"] = (int64_t) rB;
+    returnDict["fnCode"] = (int64_t) fnCode;
+    returnDict["valC"] = valC;
+    returnDict["valP"] = valP;
+    returnDict["instructionCode"] = (int64_t) instructionCode;
+
+    return returnDict;
+}
+
+std::unordered_map<string, int64_t> decode(int64_t* registers, std::unordered_map<string, int64_t> packagedValues, byte* memoryData){
+    // DECODE
+    int rA = (int) packagedValues["rA"];
+    int rB = (int) packagedValues["rB"];
+
+    int64_t valA = registers[rA];
+    int64_t valB = registers[rB];
+    int64_t valE = 0;
+
+    int stackPtr = registers[4];
+
+    int instructionCode = packagedValues["instructionCode"];
+
+    //call, pop, push, ret
+    //call
+    if(instructionCode==8){
+        valB = readDouble(memoryData,stackPtr);
+    }
+    if(instructionCode==9){
+        valA = readDouble(memoryData,stackPtr);
+        valB = readDouble(memoryData,stackPtr);
+    }
+
+    packagedValues["valA"] = valA;
+    packagedValues["valB"] = valB;
+    packagedValues["valE"] = valE;
+    packagedValues["stackPtr"] = stackPtr;
+    return packagedValues;
+
+}
+
+std::unordered_map<string, int64_t> execute(std::unordered_map<string, int64_t> packagedValues, bool* zeroFlag, bool* overflowFlag, bool* signedFlag){
+    
+    // EXECUTE
+    
+    int64_t valA = packagedValues["valA"];
+    int64_t valB = packagedValues["valB"];
+    int64_t valC = packagedValues["valC"];
+    int64_t valE = packagedValues["valE"];
+    int fnCode = (int) packagedValues["fnCode"];
+    int instructionCode = (int) packagedValues["instructionCode"];
+       
+    bool condition = false;
+    switch(instructionCode){
+        case 2:// cmov
+            if(cmpSuccessful(*zeroFlag, *overflowFlag, *signedFlag, fnCode)){
+                valB = valA;
+            }
+            break;
+        case 3:// irmovq
+            valB = valC;
+            break;
+        case 4:// rmmovq
+            valE = valB+valC;
+            break;
+        case 5:// mrmovq
+            valE = valA+valC;
+            break;
+        case 6:// OPq
+            valE = opQ(valA, valB, fnCode);
+            *zeroFlag = (valE == 0);
+            *signedFlag = (valE < 0);
+            *overflowFlag = ((valA<0) == (valB<0)) && ((valE < 0) != (valA < 0));
+            break;
+        case 7:// jXX
+            condition = cmpSuccessful(*zeroFlag, *overflowFlag, *signedFlag, fnCode);
+            break;
+        case 8:// call
+            condition = true;
+            valE = valB - 8;
+            break;
+        case 9:// ret
+            valE = valB+8;
+            condition = true;
+            break;
+        case 10:// push
+            break;
+        case 11:// pop
+            break;
+    }
+
+    packagedValues["valB"] = valB;
+    packagedValues["valE"] = valE;
+    packagedValues["condition"] = (int64_t) condition;
+    return packagedValues;
+}
+
+std::unordered_map<string, int64_t> memory(std::unordered_map<string, int64_t> packagedValues, byte* memoryData){
+    int instructionCode = (int) packagedValues["instructionCode"];
+    
+    int64_t valA = packagedValues["valA"];
+    int64_t valP = packagedValues["valP"];
+    int64_t valC = packagedValues["valC"];
+    int64_t valE = packagedValues["valE"];
+
+    // MEMORY
+    switch(instructionCode) {
+        case 4:// rmmovq
+            writeDouble(memoryData, valE, valA);
+            // memory[valE] = valA;
+            break;
+        case 8:
+            writeDouble(memoryData, valE, valP);
+            // memory[valE] = valP;
+        case 9:// ret
+            valC = readDouble(memoryData, valA);
+            break;
+        case 10:// pushq
+            // memory[valE]=valA;
+            writeDouble(memoryData, valE, valA);
+            break;
+    }
+    packagedValues["valC"] = valC;
+    return packagedValues;
+}
+std::unordered_map<string, int64_t> writeBack(std::unordered_map<string, int64_t> packagedValues, int64_t* registers){
+    //WRITE BACK
+
+    int rA = (int) packagedValues["rA"];
+    int rB = (int) packagedValues["rB"];
+
+    registers[rA] = packagedValues["valA"];
+    registers[rB] = packagedValues["valB"];
+
+    if(packagedValues["instructionCode"] == 8){
+        registers[4] = (int64_t) packagedValues["valC"];
+    }
+    if(packagedValues["instructionCode"] == 8){
+        registers[4] = (int64_t) packagedValues["valE"];
+    }
+
+    return packagedValues;
+}
+
+int64_t PC(int64_t pc, std::unordered_map<string, int64_t> packagedValues){
+
+    // PC
+    if((bool) packagedValues["condition"]){
+        pc = packagedValues["valC"];
+    }
+    else{
+        pc = packagedValues["valP"];
+    }
+    return pc;
+}
+
+
+
+
 // void runCode(byte* program){
 //     // byte* readStorage = new byte[9];
 
-//     std::unordered_map<int, int> instructToLength;
+//    std::unordered_map<int, int> instructToLength;
 //     instructToLength[0] = 1;
 //     instructToLength[1] = 1;
 //     instructToLength[2] = 2;
@@ -226,19 +367,19 @@ EMSCRIPTEN_BINDINGS(my_module) {
 //         int fnCode = (int) ((instructionNum) & ((byte) 0xF));
 //          valP = pc+instructToLength[instructionCode];
 
-//         long valC = 0;
+//         int64_t valC = 0;
 //         if(instructToMem[instructionCode]!=0){
 //             for(int i = 0; i<8; i++){
-//                 valC += (long) program[pc+instructToMem[instructionCode]+i];
+//                 valC += (int64_t) program[pc+instructToMem[instructionCode]+i];
 //                 valC *= 8;
 //             }            
 //         }
         
 //         // DECODE
         
-//         long valA = registers[rA];
-//         long valB = registers[rB];
-//         long valE = 0;
+//         int64_t valA = registers[rA];
+//         int64_t valB = registers[rB];
+//         int64_t valE = 0;
 //         bool condition = false;
 
 //         int stackPtr = registers[4];
@@ -332,22 +473,13 @@ EMSCRIPTEN_BINDINGS(my_module) {
 //     }
 // }
 
-long readDouble(byte* readFrom, long loc){
-    long r = 0;
-    for(int i = loc+8-1; i >= loc; i--){
-        r *= 2;
-        r += (long) readFrom[i];
-    }
-    return r;
-}
-
-void writeDouble(byte *writeTo, long loc, long val){
+void writeDouble(byte *writeTo, int64_t loc, int64_t val){
     for(int i = 0; i < 8; i++){
         writeTo[i + loc] = (byte) (((val) & (0xFF << (i * 8))) >> (i * 8));
     }
 }
 
-long opQ(long rA, long rB, long fnCode){
+int64_t opQ(int64_t rA, int64_t rB, int64_t fnCode){
     switch (fnCode){
         case 0:// addq
             return rA+rB;
@@ -359,6 +491,43 @@ long opQ(long rA, long rB, long fnCode){
             return rA ^ rB;
     }
     return 0;
+}
+
+//Level step, byte* memoryData, int* pc, int64_t* registers, bool* zeroFlag, bool* overflowFlag, bool* signedFlag, int* statusCode, std::unordered_map<int, int> instructToMem, std::unordered_map<int, int> instructToLength, byte* readStorage
+void step(){
+    std::unordered_map<string, int64_t> packagedValues;
+    switch(stepNum){
+        case 0:
+        //int64_t pc, int* statusCode, byte* program, std::unordered_map<int, int> instructToMem, std::unordered_map<int, int> instructToLength)
+            packagedValues = fetch(pc, &statusCode, program, instructToMem, instructToLength);
+            break;
+        case 1:
+        //decode(int64_t* registers, std::unordered_map<string, int64_t> packagedValues, byte* memoryData){
+            packagedValues = decode(registers, packagedValues, (byte *) memoryData);
+            break;
+        case 2:
+            packagedValues = execute(packagedValues, &zeroFlag, &overflowFlag, &signedFlag);
+            break;
+        case 3:
+            packagedValues = memory(packagedValues, (byte *) memoryData);
+            break;
+        case 4:
+            packagedValues = writeBack(packagedValues, registers);
+            break;
+        case 5:
+            pc = PC(pc, packagedValues);
+            break;
+    }
+    
+    render(packagedValues, registers, stepNum, memoryData, pc);
+
+    stepNum = (stepNum + 1) % 6;
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    // emscripten::function("lerp", &lerp);
+    // emscripten::function("render", render);
+    emscripten::function("step", &step);
 }
 
 bool cmpSuccessful(bool zeroFlag, bool overflowFlag, bool signedFlag, int fnCode){
---
+++ render.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <cstring>
+#include <functional>
+
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+
+const int canvasW = 1600;
+const int canvasH = 900;
+
+float rotation = 0;
+
+void rotateAt(emscripten::val ctx, int x, int y, float angle){
+    ctx.call<void>("translate", x, y);
+    ctx.call<void>("rotate", angle);
+    ctx.call<void>("translate", -x, -y);
+}
+
+std::string num2str(int64_t w, size_t hex_len = sizeof(int64_t)<<1) {
+    static const char* digits = "0123456789ABCDEF";
+    std::string rc(hex_len,'0');
+    for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
+        rc[i] = digits[(w>>j) & 0x0f];
+    return rc;
+}
+
+const int registerW = 150;
+const int registerH = canvasH / 15;
+const int registerPadding = 5;
+const int memW = 150 + registerPadding;// width for the memory
+void renderRegister(emscripten::val ctx, int x, int y, string name, string val){//int64_t registerValue
+    ctx.set("strokeStyle", "black");
+    ctx.set("lineWidth", 2);
+    ctx.call<void>("strokeRect", x + registerPadding, y + registerPadding, registerW - registerPadding*2, registerH - registerPadding*2);
+
+    // string str = int64_t2str()
+    ctx.set("fillStyle", "black");
+    if(name.length() > 5) ctx.set("font", "600 13px Ubuntu");
+    else ctx.set("font", "600 28px Ubuntu");
+    ctx.set("textAlign", "center");
+    ctx.set("textBaseline", "middle");
+    
+    ctx.call<void>("fillText", name, x + registerW/2, y + registerH/2 - 10);
+
+    ctx.set("font", "600 15px Ubuntu");
+    ctx.set("fillStyle", "black");
+    ctx.call<void>("fillText", val, x + registerW/2, y + registerH/2 + 14);
+}
+
+void renderRegisters(emscripten::val ctx, int64_t* registers){
+    const string registerNames[] = {"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14"};
+
+    int x = canvasW/2 - registerW/2;
+    int y = 0;
+    for(int i = 0; i < 15; i++){
+        renderRegister(ctx, x, y, registerNames[i], num2str(registers[i]));
+        y += registerH;
+    }
+}
+
+void renderConditionCode(emscripten::val ctx, int x, int y, string codeName, bool codeValue){
+    renderRegister(ctx, x, y, codeName, num2str(codeValue));
+}
+
+void renderPC(emscripten::val ctx, int64_t val){
+    renderRegister(ctx, 0, canvasH/2 - registerH/2, "PC", num2str(val));
+}
+
+void renderStatus(emscripten::val ctx, int x, int y, int status){
+    string statusCode = "AOK";
+    switch(status){
+        case 2:
+            statusCode = "HLT";
+        case 3:
+            statusCode = "ADR";
+        case 4:
+            statusCode = "INS";
+    }
+    renderRegister(ctx, x, y, "Status Code", statusCode);
+}
+
+int64_t readDouble(byte* readFrom, int64_t loc){
+    int64_t r = 0;
+    for(int i = loc+8-1; i >= loc; i--){
+        r *= 2 << 8;
+        r += (int64_t) readFrom[i];
+    }
+    return r;
+}
+
+void renderStackTop(emscripten::val ctx, int64_t* registers, byte* memoryData){
+    int stackPtr = registers[4];
+    
+    int start = stackPtr - 15 * 8;
+    if(start < 0) start = 0;
+    int x = canvasW - memW;
+    int y = 0;
+
+    ctx.set("strokeStyle", "black");
+    ctx.call<void>("beginPath");
+    ctx.call<void>("moveTo", x, 0);
+    ctx.call<void>("lineTo", x, canvasH);
+    ctx.call<void>("stroke");
+    ctx.call<void>("closePath");
+
+    for(int i = start; i <= stackPtr; i+=8){
+        y += registerH;
+        renderRegister(ctx, x, y, "M[" + num2str(i) + "]", num2str((int64_t) readDouble(memoryData, i)));
+    }
+}
+
+std::unordered_map<int, std::string> stepToStr;
+
+void initRender(){
+    stepToStr[0] = "Fetch";
+    stepToStr[1] = "Decode";
+    stepToStr[2] = "Execute";
+    stepToStr[3] = "Memory";
+    stepToStr[4] = "Write Back";
+    stepToStr[5] = "PC";
+}
+
+void renderStage(emscripten::val ctx, string stage){
+    // text
+    ctx.set("textAlign", "right");
+    ctx.set("textBaseline", "top");
+    ctx.set("fillStyle", "black");
+    ctx.set("font", "600 48px Ubuntu");
+    ctx.call<void>("fillText", stage, canvasW - memW - 10, 10);
+}
+
+void render(std::unordered_map<string, int64_t> packagedValues, int64_t* registers, int stepNum, byte* memoryData, int pc) {// deltaTime, use for animations
+    const auto document = emscripten::val::global("document");
+    const auto canvas = document.call<emscripten::val, std::string>("querySelector", "canvas");
+
+    auto ctx = canvas.call<emscripten::val, std::string>("getContext", "2d");
+
+    // render background
+    ctx.set("fillStyle", "white");
+    ctx.call<void>("fillRect", 0, 0, canvasW, canvasH);
+
+    renderRegisters(ctx, registers);
+    renderPC(ctx, pc);
+    renderStage(ctx, stepToStr[stepNum]);
+    renderStackTop(ctx, registers, memoryData);
+
+    // returnDict["rA"] = (int64_t) rA; //Casting to int64_t is OK here, we can just cast back to int later
+    // returnDict["rB"] = (int64_t) rB;
+    // returnDict["fnCode"] = (int64_t) fnCode
+    // returnDict["valC"] = valC;
+    // returnDict["valP"] = valB;
+    // returnDict["instructionCode"] = (int64_t) instructionCode;
+}
+
+// const auto document = emscripten::val::global("document");
+// const auto canvas = document.call<emscripten::val, std::string>("querySelector", "canvas");
+
+// auto ctx = canvas.call<emscripten::val, std::string>("getContext", "2d");
+
+// const auto width = canvas["width"].as<int>();
+// const auto height = canvas["height"].as<int>();
+
+// ctx.call<void>("clearRect", 0, 0, width, height);
+
+// // rect
+// ctx.set("fillStyle", "white");
+// ctx.call<void>("fillRect", 0, 0, width, height);
+
+// // line
+// ctx.set("strokeStyle", "black");
+// ctx.call<void>("moveTo", 0, 0);
+// ctx.call<void>("lineTo", width, height);
+// ctx.call<void>("stroke");
+
+// // text
+// ctx.set("fillStyle", "black");
+// ctx.set("font", "600 48px Ubuntu");
+// ctx.call<void, std::string>("fillText", "Hello World!", width / 2, height / 2);
+
+
+
+
+// ctx.call<void>("beginPath");
+// ctx.set("strokeStyle", "red");
+// ctx.call<void>("moveTo", x, y);
+// ctx.call<void>("lineTo", x + registerW, y + registerH);
+// ctx.call<void>("stroke");
+// ctx.call<void>("closePath");
+
+// EMSCRIPTEN_BINDINGS(my_module) {
+//     emscripten::function("render", &render);
+// }
